374a1a22d09fdf1d6f829305d72f64d870097420
Nils Gösche <n_goesche@foxitsoftware.com>
2023-02-09 17:36:04 +0100

    Add project files.

---
+++ WebMandel/Logic/IMandelbrot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebMandel.Logic
+{
+    public interface IMandelbrot
+    {
+        double XMin { get; set; }
+        double YMax { get; set; }
+        double Scale { get; set; }
+        int Iterations { get; set;}
+        byte[] GenerateMandelImage();
+    }
+}
