14c7527e1ec2949df4ffd870561bdc4b25c215cd
Cartan <cartan@cartan.de>
2023-02-09 17:31:22 +0100

    Basic Sandbox
    basic files

---
+++ Models/ContactModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebMandel.Models
+{
+    public class ContactModel
+    {
+        [StringLength(256, MinimumLength = 3), Display(Name = "Your Name")]
+        public string Name { get; set; }
+        [EmailAddress, Required, Display(Name = "Your Email")]
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        [Required, DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
---
+++ Models/MandelbrotViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using WebMandel.Logic;
+using WebMandel.Infrastructure;
+
+namespace WebMandel.Models
+{
+    public class MandelbrotViewModel
+    {
+        public double XMin { get; set; }
+        public double YMax { get; set; }
+        public double Scale { get; set; }
+        [Range(1, 10000)]
+        public int Iterations { get; set; }
+
+        public MandelbrotViewModel()
+        {
+        }
+
+        public MandelbrotViewModel(IMandelbrot mandel)
+        {
+            XMin = mandel.XMin;
+            YMax = mandel.YMax;
+            Scale = mandel.Scale;
+            Iterations = mandel.Iterations;
+        }
+
+        public void Update(IMandelbrot target)
+        {
+            target.XMin = XMin;
+            target.YMax = YMax;
+            target.Scale = Scale;
+            target.Iterations = Iterations;
+        }
+    }
+}
